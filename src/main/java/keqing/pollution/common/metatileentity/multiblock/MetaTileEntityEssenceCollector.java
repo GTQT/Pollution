551088fd6a32c702a89b096a3c701248af8e08a5
catsatori <163221322+catsatori@users.noreply.github.com>
2024-10-12 18:41:53 +0800

    前置，数值调整

--- src/main/java/keqing/pollution/common/metatileentity/multiblock/MetaTileEntityEssenceCollector.java
+++ src/main/java/keqing/pollution/common/metatileentity/multiblock/MetaTileEntityEssenceCollector.java
@@ -252,8 +252,8 @@ public class MetaTileEntityEssenceCollector extends MetaTileEntityBaseWithContro
 		int aZ = this.getPos().getZ();
 
 		// 计算灵气和污染
-		float visThisChunk = AuraHelper.getVis(this.getWorld(), new BlockPos(aX, aY, aZ));
-		float fluxThisChunk = AuraHelper.getFlux(this.getWorld(), new BlockPos(aX, aY, aZ));
+		visThisChunk = AuraHelper.getVis(this.getWorld(), new BlockPos(aX, aY, aZ));
+		fluxThisChunk = AuraHelper.getFlux(this.getWorld(), new BlockPos(aX, aY, aZ));
 
 		// 处理输入电压为 0 的情况
 		if (this.energyContainer.getInputVoltage() == 0) {
