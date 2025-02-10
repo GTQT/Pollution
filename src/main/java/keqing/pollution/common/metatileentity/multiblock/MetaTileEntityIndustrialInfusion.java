f107f75f112dfe11e5376bcff2d399e87ac04ea5
KeQing <1158677939@qq.com>
2025-02-10 14:58:08 +0800

    Update MetaTileEntityIndustrialInfusion.java

--- src/main/java/keqing/pollution/common/metatileentity/multiblock/MetaTileEntityIndustrialInfusion.java
+++ src/main/java/keqing/pollution/common/metatileentity/multiblock/MetaTileEntityIndustrialInfusion.java
@@ -80,21 +80,17 @@ import java.util.function.BooleanSupplier;
 import static keqing.pollution.api.predicate.TiredTraceabilityPredicate.CP_COIL_CASING;
 import static keqing.pollution.api.predicate.TiredTraceabilityPredicate.CP_GLASS;
 
-public class MetaTileEntityIndustrialInfusion extends MetaTileEntityBaseWithControl implements IBloomEffect, IFastRenderMetaTileEntity {
+public class MetaTileEntityIndustrialInfusion extends MetaTileEntityBaseWithControl{
     protected static final int NO_COLOR = 0;
     private final AspectList aspectList = new AspectList();
     int glass;
     int coil;
-    boolean backA;
-    int RadomTime;
     private UUID uuid = null;
     private ItemStack outputItem = ItemStack.EMPTY;
     private int tick = 0;
     private int timeAmount = 0;
     private boolean canOutput = false;
     private int eut = 0;
-    private int fusionRingColor = NO_COLOR;
-    private boolean registeredBloomRenderTicket;
 
     public MetaTileEntityIndustrialInfusion(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId);
@@ -522,192 +518,6 @@ public class MetaTileEntityIndustrialInfusion extends MetaTileEntityBaseWithCont
             this.uuid = data.getUniqueId("uid");
         this.eut = data.getInteger("eut");
 
-    }
-
-    @Override
-    protected boolean shouldShowVoidingModeButton() {
-        return false;
-    }
-
-    protected int getFusionRingColor() {
-        return this.fusionRingColor;
-    }
-
-    protected void setFusionRingColor(int fusionRingColor) {
-        if (this.fusionRingColor != fusionRingColor) {
-            this.fusionRingColor = fusionRingColor;
-        }
-    }
-
-    protected boolean hasFusionRingColor() {
-        return true;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void renderMetaTileEntity(double x, double y, double z, float partialTicks) {
-        if (this.hasFusionRingColor() && !this.registeredBloomRenderTicket) {
-            this.registeredBloomRenderTicket = true;
-            BloomEffectUtil.registerBloomRender(FusionBloomSetup.INSTANCE, getBloomType(), this, this);
-        }
-    }
-
-    @Override
-    public void update() {
-        super.update();
-        if (!backA) if (RadomTime <= 10) RadomTime++;
-        if (backA) if (RadomTime >= -10) RadomTime--;
-        if (RadomTime == 10) {
-            backA = true;
-        }
-        if (RadomTime == -10) {
-            backA = false;
-        }
-        setFusionRingColor(0xFF000000 + RadomTime * 1250 * 50);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void renderBloomEffect(BufferBuilder buffer, EffectRenderContext context) {
-        int color = this.getFusionRingColor();
-
-
-        float a = (float) (color >> 24 & 255) / 255.0F;
-        float r = (float) (color >> 16 & 255) / 255.0F;
-        float g = (float) (color >> 8 & 255) / 255.0F;
-        float b = (float) (color & 255) / 255.0F;
-        EnumFacing relativeBack = RelativeDirection.BACK.getRelativeFacing(getFrontFacing(), getUpwardsFacing(),
-                isFlipped());
-        EnumFacing.Axis axis = RelativeDirection.UP.getRelativeFacing(getFrontFacing(), getUpwardsFacing(), isFlipped())
-                .getAxis();
-
-        //if (this.isActive()) {
-        if (true) {
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    1, 1, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.Y);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    1, 1, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.X);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    1, 1, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.Z);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    RadomTime, 0.2, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.Y);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    RadomTime + 1, 0.2, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.Y);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    RadomTime + 2, 0.2, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.Y);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    3, 0.2, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.Y);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    3, 0.2, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.X);
-
-            RenderBufferHelper.renderRing(buffer,
-                    getPos().getX() - context.cameraX() + relativeBack.getXOffset() * 14 + 0.5,
-                    getPos().getY() - context.cameraY() + relativeBack.getYOffset() + 0.5,
-                    getPos().getZ() - context.cameraZ() + relativeBack.getZOffset() * 14 + 0.5,
-                    3, 0.2, 10, 20,
-                    r, g, b, a, EnumFacing.Axis.Z);
-        }
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean shouldRenderBloomEffect(EffectRenderContext context) {
-        return this.hasFusionRingColor();
-    }
-
-    @Override
-    public AxisAlignedBB getRenderBoundingBox() {
-        EnumFacing relativeRight = RelativeDirection.RIGHT.getRelativeFacing(getFrontFacing(), getUpwardsFacing(),
-                isFlipped());
-        EnumFacing relativeBack = RelativeDirection.BACK.getRelativeFacing(getFrontFacing(), getUpwardsFacing(),
-                isFlipped());
-
-        return new AxisAlignedBB(
-                this.getPos().offset(relativeBack).offset(relativeRight, 6),
-                this.getPos().offset(relativeBack, 13).offset(relativeRight.getOpposite(), 6));
-    }
-
-    @Override
-    public boolean shouldRenderInPass(int pass) {
-        return pass == 0;
-    }
-
-    @Override
-    public boolean isGlobalRenderer() {
-        return true;
-    }
-
-    @SideOnly(Side.CLIENT)
-    private static final class FusionBloomSetup implements IRenderSetup {
-
-        private static final FusionBloomSetup INSTANCE = new FusionBloomSetup();
-
-        float lastBrightnessX;
-        float lastBrightnessY;
-
-        @Override
-        public void preDraw(BufferBuilder buffer) {
-            BloomEffect.strength = (float) ConfigHolder.client.shader.fusionBloom.strength;
-            BloomEffect.baseBrightness = (float) ConfigHolder.client.shader.fusionBloom.baseBrightness;
-            BloomEffect.highBrightnessThreshold = (float) ConfigHolder.client.shader.fusionBloom.highBrightnessThreshold;
-            BloomEffect.lowBrightnessThreshold = (float) ConfigHolder.client.shader.fusionBloom.lowBrightnessThreshold;
-            BloomEffect.step = 1;
-
-            lastBrightnessX = OpenGlHelper.lastBrightnessX;
-            lastBrightnessY = OpenGlHelper.lastBrightnessY;
-            GlStateManager.color(1, 1, 1, 1);
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
-            GlStateManager.disableTexture2D();
-
-            buffer.begin(GL11.GL_QUAD_STRIP, DefaultVertexFormats.POSITION_COLOR);
-        }
-
-        @Override
-        public void postDraw(BufferBuilder buffer) {
-            Tessellator.getInstance().draw();
-
-            GlStateManager.enableTexture2D();
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lastBrightnessX, lastBrightnessY);
-        }
     }
 }
 
