a64d9cf2942090cb1e87433ffb618a6d4e2a0c67
喵呜酱睦睦子 <129736566+MeowmelMuku@users.noreply.github.com>
2025-08-05 23:45:01 +0800

    1

--- src/main/java/keqing/pollution/common/block/blocks/TestBlockInit.java
+++ src/main/java/keqing/pollution/common/block/blocks/TestBlockInit.java
@@ -1,40 +1,38 @@
 package keqing.pollution.common.block.blocks;
 
-import keqing.pollution.Pollution;
-import keqing.pollution.common.block.tile.TileEntityMagicCircle;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
 @Mod.EventBusSubscriber(modid = "pollution")
 public class TestBlockInit {
 
-        public static final TestBlock TEST_BLOCK = new TestBlock();
-        public static final Item ITEM_TEST_BLOCK = new ItemBlock(TEST_BLOCK).setRegistryName(TEST_BLOCK.getRegistryName().getPath());
+    public static final TestBlock TEST_BLOCK = new TestBlock();
+    public static final Item ITEM_TEST_BLOCK = new ItemBlock(TEST_BLOCK).setRegistryName(TEST_BLOCK.getRegistryName().getPath());
 
-        @SubscribeEvent
-        public static void registerBlock(RegistryEvent.Register<Block> event) {
-            // 和物品一样，每一个方块都有唯一一个注册名，不能使用大写字母。
-            event.getRegistry().register(TEST_BLOCK);
+    @SubscribeEvent
+    public static void registerBlock(RegistryEvent.Register<Block> event) {
+        // 和物品一样，每一个方块都有唯一一个注册名，不能使用大写字母。
+        event.getRegistry().register(TEST_BLOCK);
 
 
-        }
+    }
 
-        @SubscribeEvent
-        public static void registerItem(RegistryEvent.Register<Item> event) {
-            // 注意这个 ItemBlock 使用了和它对应的方块一样的注册名。
-            // 对于所有有物品形态的方块，其物品的注册名和它自己的注册名需要保持一致。
-            event.getRegistry().register(ITEM_TEST_BLOCK);
-        }
+    @SubscribeEvent
+    public static void registerItem(RegistryEvent.Register<Item> event) {
+        // 注意这个 ItemBlock 使用了和它对应的方块一样的注册名。
+        // 对于所有有物品形态的方块，其物品的注册名和它自己的注册名需要保持一致。
+        event.getRegistry().register(ITEM_TEST_BLOCK);
+    }
+
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public static void onModelReg(ModelRegistryEvent event) {
@@ -43,7 +41,6 @@ public class TestBlockInit {
 
 
     }
-
 
 
 }
